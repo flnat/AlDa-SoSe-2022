ec81b7d7bd9cd2e4cbd20077cdf6d5361a7f5709
Florian Natterer <natterer.flo99@gmail.com>
2022-04-10 16:11:33 +0200

    Generische Methoden aus InsertionSort in eigene abstrakte Klasse SortingAlgorithm verschoben, damit Methoden auch für andere Methoden benutzbar sind.

--- src/Aufgabe2/InsertionSort.java
+++ src/Aufgabe2/InsertionSort.java
@@ -1,6 +1,6 @@
 package Aufgabe2;
 
-public class InsertionSort {
+public class InsertionSort extends SortingAlgorithm {
     //Implementiert nach Sedgewick - Algorithms Kapitel 2
 
     public static <T extends Comparable<T>> T[] sort(T[] a) {
@@ -11,24 +11,5 @@ public class InsertionSort {
             }
         }
         return a;
-    }
-
-    private static <T extends Comparable<T>> boolean less(T v, T w) {
-        return v.compareTo(w) < 0;
-    }
-
-    private static <T extends Comparable<T>> void exchange(T[] a, int i, int j) {
-        T t = a[i];
-        a[i] = a[j];
-        a[j] = t;
-    }
-
-    private static <T extends Comparable<T>>  boolean isSorted(T[] a) {
-        for (int i = 0; i < a.length; i++) {
-            if (less(a[i], a[i - 1])) {
-                return false;
-            }
-        }
-        return true;
     }
 }
---
+++ src/Aufgabe2/SortingAlgorithm.java
@@ -0,0 +1,16 @@
+package Aufgabe2;
+
+public abstract class SortingAlgorithm {
+
+    protected static <T extends Comparable<T>> boolean less(T v, T w) {
+        return v.compareTo(w) < 0;
+    }
+
+    protected static <T extends Comparable<T>> void exchange(T[] a, int i, int j) {
+        T t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+
+}
